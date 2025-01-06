03fe14d20d7c6438a0274d7fa8cadc7985fb7bc3
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2025-01-05 21:13:20 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -49,24 +49,30 @@ if mode == "Single Value":
     if st.button("Calculate"):
         
         v = calc(x, r, f)
+        v = str(v)
 
-        if v == None:
+        if v == "nan":
             st.write("The result of the given values cannot be obtained. Please enter a valid set of values.")
 
-        elif unit == "M/S":
-            st.write(f"**Maximum Speed:** *{round(v, 3)} m/s*")
+        else:
 
-        elif unit == "KM/H":
-            st.write(f"**Maximum Speed:** *{round(v * 3.6, 3)} km/h*")
+            v = float(v)
 
-        elif unit == "MPH":
-            st.write(f"**Maximum Speed:** *{round(v * 3.6 / 1.609, 3)} mph*")
+            if unit == "M/S":
+                st.write(f"**Maximum Speed:** *{round(v, 3)} m/s*")
+
+            elif unit == "KM/H":
+                st.write(f"**Maximum Speed:** *{round(v * 3.6, 3)} km/h*")
+
+            elif unit == "MPH":
+                st.write(f"**Maximum Speed:** *{round(v * 3.6 / 1.609, 3)} mph*")
 
 else:
 
     st.title(f"{mode} View")
 
     cond = sidebar.radio("Road Conditions (with a regular vehicle, except for F1):", ["Formula 1 (F1 racecar and track)", "Dry Road", "Wet Road", "Snowy Road", "Icy Road"])
+    f = 1.7
 
     if cond == "Formula 1 - Dry Road (F1 racecar and track)":
         f = 1.7
@@ -84,7 +90,6 @@ else:
         f = 0.15
 
     r = sidebar.number_input("Turn Radius (meters):", value=100)
-    f = sidebar.number_input("Friction Coefficient:", value=0.9)
     minb = sidebar.number_input("Minimum Bank Angle:", value=-41)
     maxb = sidebar.number_input("Maximum Bank Angle:", value=45)
 
@@ -123,7 +128,7 @@ else:
         plt.xlabel("Bank Angle (degrees)")
         plt.ylabel("Maximum Speed (m/s)")
 
-        if c4.checkbox("Vertical Line at `x=0`", True):
+        if c4.checkbox("Vertical Line at  `x = 0`", True):
             plt.axvline(0, color="red")
 
         st.pyplot(fig)
@@ -140,6 +145,10 @@ else:
 
         elif unit == "MPH":
             y = [round(calc(a, r, f), 3) * 3.6 / 1.609 for a in x]
+        
+        for i in range(len(y)):
+            if str(y[i]) == "nan":
+                y[i] = "Cannot Be Calculated"
 
         if unit == "M/S":
             
@@ -160,6 +169,6 @@ else:
             data = {
                 "Bank Angle (degrees)": x,
                 "Max. Speed (MPH)": y
-            }
+            }  
 
         st.dataframe(data, use_container_width=True, hide_index=True)
